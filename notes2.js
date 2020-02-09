0d7d243f79e4d19c6b53b63ecd0e952affb5158c
github username <github email address>
2020-02-08 23:49:26 -0500

    Tweak notes2

--- notes2.js
+++ notes2.js
@@ -4,9 +4,7 @@ const solution = (arr) => {
    const reducer = (accumulator, currentValue) => accumulator + currentValue;
    left = [...arr].slice(0, arr.length / 2).length
    right = [...arr].slice(arr.length / 2, arr.length).length
-    
- 
-    
+
    if(arr.length === 0){
        return '';
    } else if (left === right){
